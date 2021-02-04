898790766099c7fb498db814f4ad5d4e4509e131
BennyBlockchain <ben@styng.social>
2021-02-04 12:05:49 -0500

    Styled Accordion categories

--- components/AccordionItem/AccordionItem.js
+++ components/AccordionItem/AccordionItem.js
@@ -1,15 +1,23 @@
 import { Card, Accordion } from "react-bootstrap";
 
+import styles from "./AccordionItem.module.scss";
+
 const AccordionItem = (category) => {
   const { action, name, children } = category;
   return (
     <>
       <Card>
-        <Accordion.Toggle as={Card.Header} eventKey={action}>
+        <Accordion.Toggle
+          as={Card.Header}
+          eventKey={action}
+          className={styles.biggerFont}
+        >
           {name}
         </Accordion.Toggle>
         <Accordion.Collapse eventKey={action}>
-          <Card.Body>{children}</Card.Body>
+          <div className="d-flex flex-row flex-wrap justify-content-start">
+            {children}
+          </div>
         </Accordion.Collapse>
       </Card>
     </>
