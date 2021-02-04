1595fdb4c5e9dd03a428eea3d68ed59849aab585
BennyBlockchain <ben@styng.social>
2021-02-03 21:08:57 -0500

    Added sass support and custom bootstrap variables

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import "../styles/globals.css";
-import "../styles/custom.scss";
-
 import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/custom.scss";
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
