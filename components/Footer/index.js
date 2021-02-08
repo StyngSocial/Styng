1415eefcb252ad6c7704e30981574e5b0c5d7b6e
BennyBlockchain <ben@styng.social>
2021-02-08 15:20:12 -0500

    Added FontAwesome icons to footer with link to social pages.

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,16 +1,44 @@
 import styles from "./Footer.module.scss";
-import { Col, Row } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 
+import {
+  faLinkedin,
+  faMedium,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Footer = () => {
   return (
     <>
       <footer className={styles.footer}>
         <hr className="my-8" />
-        <Row className="justify-content-end">
-          <Col lg={2}>Instagram</Col>
-          <Col lg={2}>LinkedIn</Col>
-          <Col lg={2}>Medium</Col>
-        </Row>
+        <Container>
+          <a href="https://www.instagram.com/styng.social/" target="_blank">
+            <FontAwesomeIcon
+              className={styles.footerIcon}
+              icon={faInstagram}
+              size="lg"
+            ></FontAwesomeIcon>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/styng-social-b23337201/"
+            target="_blank"
+          >
+            <FontAwesomeIcon
+              className={styles.footerIcon}
+              icon={faLinkedin}
+              size="lg"
+            ></FontAwesomeIcon>
+          </a>
+          <a href="https://styng-social.medium.com/" target="_blank">
+            <FontAwesomeIcon
+              className={styles.footerIcon}
+              icon={faMedium}
+              size="lg"
+            ></FontAwesomeIcon>
+          </a>
+          <span className="float-end">Styng Social LLC 2021 &#169;</span>
+        </Container>
       </footer>
     </>
   );
