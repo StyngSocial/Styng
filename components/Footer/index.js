206a83ffd76ba4c3f2ad71568145fae984ba07f6
Benny Blockchain <ben@styng.social>
2021-02-10 19:35:53 -0500

    Added changing background

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -10,35 +10,34 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Footer = () => {
   return (
     <>
-      <footer className={styles.footer}>
+      <footer className={`${styles.footer} mx-4`}>
         <hr className="my-8" />
-        <Container>
-          <a href="https://www.instagram.com/styng.social/" target="_blank">
-            <FontAwesomeIcon
-              className={styles.footerIcon}
-              icon={faInstagram}
-              size="lg"
-            ></FontAwesomeIcon>
-          </a>
-          <a
-            href="https://www.linkedin.com/in/styng-social-b23337201/"
-            target="_blank"
-          >
-            <FontAwesomeIcon
-              className={styles.footerIcon}
-              icon={faLinkedin}
-              size="lg"
-            ></FontAwesomeIcon>
-          </a>
-          <a href="https://styng-social.medium.com/" target="_blank">
-            <FontAwesomeIcon
-              className={styles.footerIcon}
-              icon={faMedium}
-              size="lg"
-            ></FontAwesomeIcon>
-          </a>
-          <span className="float-end">Styng Social LLC 2021 &#169;</span>
-        </Container>
+
+        <a href="https://www.instagram.com/styng.social/" target="_blank">
+          <FontAwesomeIcon
+            className={styles.footerIcon}
+            icon={faInstagram}
+            size="lg"
+          ></FontAwesomeIcon>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/styng-social-b23337201/"
+          target="_blank"
+        >
+          <FontAwesomeIcon
+            className={styles.footerIcon}
+            icon={faLinkedin}
+            size="lg"
+          ></FontAwesomeIcon>
+        </a>
+        <a href="https://styng-social.medium.com/" target="_blank">
+          <FontAwesomeIcon
+            className={styles.footerIcon}
+            icon={faMedium}
+            size="lg"
+          ></FontAwesomeIcon>
+        </a>
+        <span className="float-end">Styng Social LLC 2021 &#169;</span>
       </footer>
     </>
   );
--- pages/index.js
+++ pages/index.js
@@ -64,9 +64,8 @@ export default function Home() {
               let's fix it.
             </h1>
             <p>
-              A social media platform built for the user. Styng bridges modern
-              technologies with social media. Enabling users to generate income
-              for using and engaging on the platform.
+              A social media platform built for the user. Enabling users to
+              generate income for using and engaging on the platform.
             </p>
             <hr className="my-4" />
             <Button variant="primary" size="lg" onClick={handleModal}>
@@ -78,121 +77,138 @@ export default function Home() {
           </Col>
         </Row>
       </Container>
-      <Container className={styles.solutions} id="about">
-        <Row className="justify-content-center align-items-evenly">
-          <Col lg={5} md={12} className="me-3">
-            <h2 className="primary-color">Social media is exploitive</h2>
-            <hr className="my-4" />
-            <li className="h4">Broken Business Model</li>
-            <p>
-              Top social media platforms generate more than 98% of their revenue
-              from user data collection giving them trillions in market cap.
-              Users receive no benefit or consent for their personal information
-              to be sold.
-            </p>
-            <li className="h4">Behavior Changing</li>
-            <p>
-              Social media has been made addictive and manipulative to benefit
-              maximum profits for the platform.
-            </p>
-            <li className="h4">Polarized</li>
-            <p>
-              Content curation algorithms have segmented users into echo
-              chambers, stripping the ability to empathize and communicate.
-            </p>
-          </Col>
-          <Col lg={5} md={12} className="ms-3">
-            <h2 className={`primary-color ${styles.ourSolution}`}>
-              Our solution
-            </h2>
-            <hr className="my-4" />
-            <p>
-              Styng is flipping the business model of social media. Using
-              technology that has the ability to compensate users for their
-              engagement on Styng.
-            </p>
-            <p>
-              Styng is creating a platform with a user-first approach. Instead
-              of manipulative closed source algorithms, Styng uses community
-              curated algorithms. Users also have the ability to generate
-              monetary benefit for engaging with other users and curating
-              content.
-            </p>
-            <p>
-              Traditional social media treats their users as a product instead
-              of users. Big Tech sells their collection of hyper specific user
-              profiles to third parties without user consent. The relationship
-              between targeting certain individuals and algorithmic curation has
-              caused social media feeds to be manipulative and behavior
-              changing.
-            </p>
-          </Col>
-        </Row>
-      </Container>
-      <Container className={styles.business}>
-        <Row className="jusitfy-content-center">
-          <Col lg={10} className="mx-auto">
-            <h2 className="primary-color">Breaking the relationship</h2>
-            <hr className="my-4" />
-            <p>
-              Social media relies on collecting user personal information to
-              sell to third parties. This relationship exclusively brings value
-              to third parties and platforms leaving the user without retaining
-              any value of their information. Styng is breaking away from Web
-              2.0 into a new generation of user benefited internet, Web 3.0.
-            </p>
-          </Col>
-        </Row>
-        <Row className={styles.webGen}>
-          <Col lg={6} md={12} className="text-center">
-            <h3 className="primary-text">Web 2.0</h3>
-            <Image src={Web2} width={500} height={500}></Image>
-          </Col>
-          <Col lg={6} md={12} className="text-center">
-            <h3 className="primary-text">Web 3.0</h3>
-            <Image src={Web3} height={500} width={500}></Image>
-          </Col>
-        </Row>
-      </Container>
-      <Container className={styles.research} id="whitepaper">
-        <Row className="justify-content-evenly">
-          <Col lg={5} md={12} className="text-center mx-auto position-relative">
-            <Image src={ExecSum} height={75} width={75}></Image>
-            <h2 className="primary-color">Executive Summary</h2>
-            <hr className="my-4" />
-            <p>
-              The executive summary is a high level blueprint for Styng. This
-              document will include information outside of the technical
-              objective. It contains competitive analysis, inspiration, business
-              model, and projections.
-            </p>
-            <Button
-              variant="primary"
-              disabled
-              className="position-absolute bottom-0 translate-middle"
-            >
-              Executive Summary
-            </Button>
-          </Col>
-          <Col lg={5} md={12} className="text-center mx-auto position-relative">
-            <Image src={Whitepaper} height={75} width={75}></Image>
-            <h2 className="primary-color">Whitepaper</h2>
-            <hr className="my-4" />
-            <p>
-              A whitepaper is a technical document describing how Styng, the
-              platform, works. It will include token economic information, token
-              distribution, implementation, and execution.
-            </p>
+      <div className={styles.skewBgR}>
+        <Container className={`${styles.solutions} mw-100`} id="about">
+          <Row className="justify-content-center align-items-evenly">
+            <Col lg={5} md={12} className="me-lg-3">
+              <h2 className="primary-color">Social media is exploitive</h2>
+              <hr className="my-4" />
+              <li className="h4">Broken Business Model</li>
+              <p>
+                Top social media platforms generate more than 98% of their
+                revenue from user data collection giving them trillions in
+                market cap. Users receive no benefit or consent for their
+                personal information to be sold.
+              </p>
+              <li className="h4">Behavior Changing</li>
+              <p>
+                Social media has been made addictive and manipulative to benefit
+                maximum profits for the platform.
+              </p>
+              <li className="h4">Polarized</li>
+              <p>
+                Content curation algorithms have segmented users into echo
+                chambers, stripping the ability to empathize and communicate.
+              </p>
+            </Col>
+            <Col lg={5} md={12} className="ms-lg-3">
+              <h2 className={`primary-color ${styles.ourSolution}`}>
+                Our solution
+              </h2>
+              <hr className="my-4" />
+              <p>
+                Styng is flipping the business model of social media. Using
+                technology that has the ability to compensate users for their
+                engagement on Styng.
+              </p>
+              <p>
+                Styng is creating a platform with a user-first approach. Instead
+                of manipulative closed source algorithms, Styng uses community
+                curated algorithms. Users also have the ability to generate
+                monetary benefit for engaging with other users and curating
+                content.
+              </p>
+              <p>
+                Traditional social media treats their users as a product instead
+                of users. Big Tech sells their collection of hyper specific user
+                profiles to third parties without user consent. The relationship
+                between targeting certain individuals and algorithmic curation
+                has caused social media feeds to be manipulative and behavior
+                changing.
+              </p>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <Container className={`${styles.business} mw-100`}>
+        <div className={styles.scewBgL}>
+          <Row className="jusitfy-content-center">
+            <Col lg={10} className="mx-auto">
+              <h2 className="primary-color">Breaking the relationship</h2>
+              <hr className="my-4" />
+              <p>
+                Social media relies on collecting user personal information to
+                sell to third parties. This relationship exclusively brings
+                value to third parties and platforms leaving the user without
+                retaining any value of their information. Styng is breaking away
+                from Web 2.0 into a new generation of user benefited internet,
+                Web 3.0.
+              </p>
+            </Col>
+          </Row>
 
-            <Button
-              variant="primary"
-              disabled
-              className="position-absolute bottom-0 translate-middle"
+          <Row className={styles.webGen}>
+            <Col lg={6} md={12} className="text-center">
+              <h3 className="primary-text">Web 2.0</h3>
+              <Image src={Web2} width={500} height={500}></Image>
+            </Col>
+            <Col lg={6} md={12} className="text-center">
+              <h3 className="primary-text">Web 3.0</h3>
+              <Image src={Web3} height={500} width={500}></Image>
+            </Col>
+          </Row>
+        </div>
+      </Container>
+      <Container className={`${styles.research} mw-100`} id="whitepaper">
+        <div className={styles.scewBgL}>
+          <Row className="justify-content-evenly">
+            <Col
+              lg={5}
+              md={12}
+              className="text-center mx-auto position-relative"
             >
-              Whitepaper
-            </Button>
-          </Col>
-        </Row>
+              <Image src={ExecSum} height={75} width={75}></Image>
+              <h2 className="primary-color">Executive Summary</h2>
+              <hr className="my-4" />
+              <p>
+                The executive summary is a high level blueprint for Styng. This
+                document will include information outside of the technical
+                objective. It contains competitive analysis, inspiration,
+                business model, and projections.
+              </p>
+              <Button
+                variant="dark"
+                disabled
+                className="position-absolute bottom-0 translate-middle"
+              >
+                Executive Summary
+              </Button>
+            </Col>
+
+            <Col
+              lg={5}
+              md={12}
+              className="text-center mx-auto position-relative"
+            >
+              <Image src={Whitepaper} height={75} width={75}></Image>
+              <h2 className="primary-color">Whitepaper</h2>
+              <hr className="my-4" />
+              <p>
+                A whitepaper is a technical document describing how Styng, the
+                platform, works. It will include token economic information,
+                token distribution, implementation, and execution.
+              </p>
+
+              <Button
+                variant="dark"
+                disabled
+                className="position-absolute bottom-0 translate-middle"
+              >
+                Whitepaper
+              </Button>
+            </Col>
+          </Row>
+        </div>
       </Container>
       <Modal show={show} onHide={handleModal} centered>
         <Modal.Header>
